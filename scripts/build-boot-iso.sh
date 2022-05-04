3203b684513b8009ad45770e9799225f6a4583c2
Daniel Schier <dschier@while-true-do.io>
2022-05-04 18:23:28 +0200

    initial baseline

---
+++ scripts/build-boot-iso.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# Vars
+
+WORKDIR=""
+SOURCE_ISO_URL=""
+DEST_ISO_NAME=""
+
+# Functions
+
+function download_iso() {
+  echo "foo"
+}
+
+function mount_iso() {
+  echo "bar"
+}
+
+function copy_tree() {
+  echo "baz"
+}
+
+function copy_kickstart() {
+  echo "qux"
+}
+
+function unmount_iso() {
+  echo "quux"
+}
+
+function build_iso() {
+  echo "corge"
+}
+
+function sign_iso() {
+  echo "grault"
+}
+
+download_iso
+mount_iso
+copy_tree
+copy_kickstart
+unmount_iso
+build_iso
+sign_iso
